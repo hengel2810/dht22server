dfa65ffa4a1df52c4a6061ec8a2965e1af732216
Henrik Engelbrink <hengel2810@gmail.com>
2017-12-10 18:26:13 +0100

    Init commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const Realm = require("realm")
+
+const DHT22ValueSchema = {
+	name: "DHT22Value",
+	properties: {
+	  	sensor:  "string",
+		temperature: "double",
+		humidity: "double",
+		timestamp: "double"
+	}
+};
+
+let realm = new Realm({path:"../dht22Data/dht22.realm", schema: [DHT22ValueSchema]});
+
+app.get('/dht22/values', function(req, res) {
+	let values = realm.objects('DHT22Value');
+	let sortedValues = values.sorted('timestamp', true);
+	var valueArray = [];
+	sortedValues.forEach(function(value, index) {
+		valueArray.push(value);
+	});
+	res.status(200).json(valueArray);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
