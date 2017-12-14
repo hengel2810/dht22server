31eca4966840d3a86f3a1022570a1a2aca45f1f5
Henrik Engelbrink <hengel2810@gmail.com>
2017-12-14 13:57:39 +0100

    Updated routes

--- src/index.js
+++ src/index.js
@@ -14,21 +14,18 @@ const DHT22ValueSchema = {
 
 let realm = new Realm({path:"./dht22Data/dht22.realm", schema: [DHT22ValueSchema]});
 
-app.get('/dht22/values', function(req, res) {
-	let valueArray = valuesFromRealm(100);
-	res.status(200).json(valueArray);
-});
-
 app.get('/dht22/values/:count', function(req, res) {
 	var countStr = req.params.count;
 	var count = 100;
-	try {
+	if(countStr) {
 		count = parseInt(countStr);
 	}
-	catch {
-		res.status(400);
-	}
 	let valueArray = valuesFromRealm(count);
+	res.status(200).json(valueArray);
+});
+
+app.get('/dht22/values/all', function(req, res) {
+	let valueArray = valuesFromRealm();
 	res.status(200).json(valueArray);
 });
 
@@ -39,9 +36,11 @@ app.listen(3000, '0.0.0.0');
 function valuesFromRealm(count) {
 	let values = realm.objects('DHT22Value');
 	let sortedValues = values.sorted('timestamp', true);
-	let limitedValues = sortedValues.slice(0, 100);
+	if(count) {
+		sortedValues = sortedValues.slice(0, count);
+	}
 	var valueArray = [];
-	limitedValues.forEach(function(value, index) {
+	sortedValues.forEach(function(value, index) {
 		valueArray.push(value);
 	});
 	return valueArray;
