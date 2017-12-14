27bbde2084354a85ac0b6d8ffeceb469aa1b228b
Henrik Engelbrink <hengel2810@gmail.com>
2017-12-14 13:41:05 +0100

    Added route with count

--- src/index.js
+++ src/index.js
@@ -15,6 +15,28 @@ const DHT22ValueSchema = {
 let realm = new Realm({path:"./dht22Data/dht22.realm", schema: [DHT22ValueSchema]});
 
 app.get('/dht22/values', function(req, res) {
+	let valueArray = valuesFromRealm(100);
+	res.status(200).json(valueArray);
+});
+
+app.get('/dht22/values/:count', function(req, res) {
+	var countStr = req.params.count;
+	var count = 100;
+	try {
+		count = parseInt(countStr);
+	}
+	catch {
+		res.status(400);
+	}
+	let valueArray = valuesFromRealm(count);
+	res.status(200).json(valueArray);
+});
+
+
+app.listen(3000, '0.0.0.0');
+
+
+function valuesFromRealm(count) {
 	let values = realm.objects('DHT22Value');
 	let sortedValues = values.sorted('timestamp', true);
 	let limitedValues = sortedValues.slice(0, 100);
@@ -22,7 +44,5 @@ app.get('/dht22/values', function(req, res) {
 	limitedValues.forEach(function(value, index) {
 		valueArray.push(value);
 	});
-	res.status(200).json(valueArray);
-});
-
-app.listen(3000, '0.0.0.0');
+	return valueArray;
+}
